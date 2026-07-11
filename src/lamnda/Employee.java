fbc2aa60c8c1fae445b0cd9e040b2bc889ea6ede
Raimzhanov <Raimzhanov@lanit.ru>
2026-07-11 19:26:26 +0300

    Stream API

--- src/lamnda/Employee.java
+++ src/lamnda/Employee.java
@@ -2,11 +2,26 @@ package lamnda;
 
 class Employee {
     private String name;
+    private String secondName;
+    private int age;
+    private String address;
+    private String phone;
+    private String position;
     private double salary;
 
     public Employee(String name, double salary) {
         this.salary = salary;
         this.name = name;
+    }
+
+    public Employee(String name, String secondName, int age, String address, String phone, String position, double salary) {
+        this.name = name;
+        this.secondName = secondName;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+        this.position = position;
+        this.salary = salary;
     }
 
     public String getName() {
@@ -23,5 +38,45 @@ class Employee {
 
     public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 }
